caea35c7fb6ab88adc87510754173696d11166b7
Harri Lehtola <peruukki@hotmail.com>
2015-01-25 20:52:58 +0100

    Create initial scoreboard tables

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yatzy Scoreboard
+
+Keep track of your [Yatzy](http://en.wikipedia.org/wiki/Yatzy) game scores in the browser.
---
+++ js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+(function () {
+
+  var COLUMN_HEADERS = [ '', 'Player 1', 'Player 2' ];
+  var ROW_HEADERS_UPPER_SECTION = [ 'Ones', 'Twos', 'Threes', 'Fours', 'Fives', 'Sixes', 'Subtotal' ];
+  var ROW_HEADERS_LOWER_SECTION = [ 'Pair', 'Two pairs', 'Three of a kind', 'Four of a kind', 'Five of a kind',
+                                    'Small straight', 'Large straight', 'Full house', 'Chance', 'Yatzy', 'TOTAL' ];
+
+  $(document).ready(function () {
+    addTableRows('#upper-section', COLUMN_HEADERS, ROW_HEADERS_UPPER_SECTION);
+    addTableRows('#lower-section', COLUMN_HEADERS, ROW_HEADERS_LOWER_SECTION);
+  });
+
+  function addTableRows(tableSelector, columnHeaders, rowHeaders) {
+    addColumnHeaderRow(tableSelector + ' thead', columnHeaders);
+    addBodyRows(tableSelector + ' tbody', rowHeaders, columnHeaders);
+  }
+
+  function addColumnHeaderRow(tableHeadElement, columnHeaders) {
+    $(tableHeadElement).append($('<tr>')
+      .append(columnHeaders.map(function (header) {
+        return '<th>' + header + '</th>';
+      })));
+  }
+
+  function addBodyRows(tableBodyElement, rowHeaders, columnHeaders) {
+    var headerElements = rowHeaders.map(function (header) {
+      return '<th>' + header + '</th>';
+    });
+    var emptyRowCells = _(columnHeaders).drop(1).map(function (header) {
+      return '<td>';
+    }).valueOf();
+
+    headerElements.forEach(function (headerElement) {
+      $(tableBodyElement).append($('<tr>')
+        .append(headerElement)
+        .append(emptyRowCells));
+    });
+  }
+
+})();
