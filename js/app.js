a741841bbb100d1de036f4c2c86fc21d7dbeb7a8
Harri Lehtola <peruukki@hotmail.com>
2015-01-29 22:31:04 +0100

    Set cell specific input steps and maximum values

--- js/app.js
+++ js/app.js
@@ -3,23 +3,37 @@
 (function () {
 
   var COLUMN_HEADERS = [ 'Player 1', 'Player 2' ];
-  var ROW_HEADERS_UPPER_SECTION = [ 'Ones', 'Twos', 'Threes', 'Fours', 'Fives', 'Sixes', 'Subtotal' ];
-  var ROW_HEADERS_LOWER_SECTION = [ 'Pair', 'Two pairs', 'Three of a kind', 'Four of a kind', 'Five of a kind',
-                                    'Small straight', 'Large straight', 'Full house', 'Chance', 'Yatzy', 'TOTAL' ];
 
+  var UPPER_SECTION_INPUTS = [
+    Input('Ones', 1, 5), Input('Twos', 2, 10), Input('Threes', 3, 15),
+    Input('Fours', 4, 20), Input('Fives', 5, 25), Input('Sixes', 6, 30)
+  ];
+  var UPPER_SECTION_TOTAL_LABEL = 'Subtotal';
+
+  var LOWER_SECTION_INPUTS = [
+    Input('Pair', 2, 12), Input('Two pairs', 2, 24),
+    Input('Three of a kind', 3, 18), Input('Four of a kind', 4, 24), Input('Five of a kind', 5, 30),
+    Input('Small straight', 15, 15), Input('Large straight', 20, 20),
+    Input('Full house', 1, 30), Input('Chance', 1, 30), Input('Yatzy', 5, 80)
+  ];
+  var LOWER_SECTION_TOTAL_LABEL = 'TOTAL';
 
   $(document).ready(function () {
-    addTableRows('#upper-section', COLUMN_HEADERS, ROW_HEADERS_UPPER_SECTION);
-    addTableRows('#lower-section', COLUMN_HEADERS, ROW_HEADERS_LOWER_SECTION);
+    addTableRows('#upper-section', COLUMN_HEADERS, UPPER_SECTION_INPUTS, UPPER_SECTION_TOTAL_LABEL);
+    addTableRows('#lower-section', COLUMN_HEADERS, LOWER_SECTION_INPUTS, LOWER_SECTION_TOTAL_LABEL);
 
     observePlayerNameInput();
     observeScoreInput();
   });
 
 
-  function addTableRows(tableSelector, columnHeaders, rowHeaders) {
+  function Input(label, step, max) {
+    return { label: label, step: step, max: max };
+  }
+
+  function addTableRows(tableSelector, columnHeaders, rowHeaders, totalLabel) {
     addColumnHeaderRow(tableSelector + ' thead', columnHeaders);
-    addBodyRows(tableSelector + ' tbody', rowHeaders, columnHeaders);
+    addBodyRows(tableSelector + ' tbody', columnHeaders, rowHeaders, totalLabel);
   }
 
   function addColumnHeaderRow(tableHeadElement, columnHeaders) {
@@ -30,24 +44,23 @@
       })));
   }
 
-  function addBodyRows(tableBodyElement, rowHeaders, columnHeaders) {
-    var headerElements = rowHeaders.map(function (header) {
-      return '<th>' + header + '</th>';
+  function addBodyRows(tableBodyElement, columnHeaders, rowHeaders, totalLabel) {
+    // Score input cells
+    rowHeaders.forEach(function (header) {
+      var scoreInputCells = columnHeaders.map(function () {
+        return '<td><input type="number" min="0" max="' + header.max + '" step="' + header.step + '" class="score"></input></td>';
+      });
+      $(tableBodyElement).append($('<tr>')
+        .append('<th>' + header.label + '</th>')
+        .append(scoreInputCells));
     });
-    var scoreInputCells = columnHeaders.map(function () {
-      return '<td><input type="number" min="0" max="80" class="score"></input></td>';
-    });
+
+    // Total cells
     var totalScoreCells = columnHeaders.map(function () {
       return '<td><input type="number" min="0" class="total" readonly></input></td>';
     });
-
-    _(headerElements).take(headerElements.length - 1).forEach(function (headerElement) {
-      $(tableBodyElement).append($('<tr>')
-        .append(headerElement)
-        .append(scoreInputCells));
-    });
     $(tableBodyElement).append($('<tr>')
-      .append(_(headerElements).last())
+      .append('<th>' + totalLabel + '</th>')
       .append(totalScoreCells));
   }
 
