f081de4d4dd764c6d408679fed9beeaa09cae897
Harri Lehtola <peruukki@hotmail.com>
2015-01-31 16:41:35 +0100

    Show score difference per row next to the bigger score cell

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
 
 (function () {
 
-  var COLUMN_HEADERS = [ 'Player 1', 'Player 2' ];
+  var COLUMN_HEADERS = [ 'Name', 'Name' ];
 
   var UPPER_SECTION_INPUTS = [
     Input('Ones', 1, 5), Input('Twos', 2, 10), Input('Threes', 3, 15),
     Input('Fours', 4, 20), Input('Fives', 5, 25), Input('Sixes', 6, 30)
   ];
-  var UPPER_SECTION_TOTAL_LABEL = 'Subtotal';
+  var UPPER_SECTION_TOTAL_LABEL = 'Total';
 
   var LOWER_SECTION_INPUTS = [
     Input('Pair', 2, 12), Input('Two pairs', 2, 24),
@@ -18,18 +18,25 @@
   ];
   var LOWER_SECTION_TOTAL_LABEL = 'TOTAL';
 
+
   $(document).ready(function () {
     addTableRows('#upper-section', COLUMN_HEADERS, UPPER_SECTION_INPUTS, UPPER_SECTION_TOTAL_LABEL);
     addTableRows('#lower-section', COLUMN_HEADERS, LOWER_SECTION_INPUTS, LOWER_SECTION_TOTAL_LABEL);
 
     observePlayerNameInput();
-    observeScoreInput();
+    var scoreObservables = observeScoreInput();
+    observeScoreDifferences(scoreObservables[0], scoreObservables[1]);
   });
 
 
   function Input(label, step, max) {
     return { label: label, step: step, max: max };
   }
+
+  function RowScoreChange(score1, score2) {
+    return { score1: score1, score2: score2 };
+  }
+
 
   function addTableRows(tableSelector, columnHeaders, rowHeaders, totalLabel) {
     addColumnHeaderRow(tableSelector + ' thead', columnHeaders);
@@ -39,29 +46,34 @@
   function addColumnHeaderRow(tableHeadElement, columnHeaders) {
     $(tableHeadElement).append($('<tr>')
       .append('<th>')
-      .append(columnHeaders.map(function (header) {
-        return '<th><input type="text" class="player" placeholder="' + header + '"></input></th>';
+      .append(columnHeaders.map(function (header, index) {
+        return '<th class="player-' + (index + 1) + '">' +
+          '<input type="text" class="player" placeholder="' + header + '"></input></th>';
       })));
   }
 
   function addBodyRows(tableBodyElement, columnHeaders, rowHeaders, totalLabel) {
     // Score input cells
     rowHeaders.forEach(function (header) {
-      var scoreInputCells = columnHeaders.map(function () {
-        return '<td><input type="number" min="0" max="' + header.max + '" step="' + header.step + '" class="score"></input></td>';
-      });
+      var input = '<input type="number" min="0" max="' + header.max + '" step="' + header.step + '" class="score"></input>';
+      var score1Diff = '<span class="score-diff player-1"></span>';
+      var score2Diff = '<span class="score-diff player-2"></span>';
+
       $(tableBodyElement).append($('<tr>')
         .append('<th>' + header.label + '</th>')
-        .append(scoreInputCells));
+        .append('<td class="player-1">' + score1Diff + input + '</td>')
+        .append('<td class="player-2">' + input + score2Diff + '</td>'));
     });
 
     // Total cells
-    var totalScoreCells = columnHeaders.map(function () {
-      return '<td><input type="number" min="0" class="total" readonly></input></td>';
-    });
+    var totalScoreInput = '<input type="number" min="0" class="total" readonly></input>';
+    var totalScore1Diff = '<span class="score-diff player-1"></span>';
+    var totalScore2Diff = '<span class="score-diff player-2"></span>';
+
     $(tableBodyElement).append($('<tr>')
       .append('<th>' + totalLabel + '</th>')
-      .append(totalScoreCells));
+      .append('<td class="player-1">' + totalScore1Diff + totalScoreInput + '</td>')
+      .append('<td class="player-2">' + totalScoreInput + totalScore2Diff + '</td>'));
   }
 
 
@@ -86,8 +98,12 @@
 
 
   function observeScoreInput() {
-    var upperSectionTotalObservables = bindScoreData('#upper-section');
-    bindScoreData('#lower-section', upperSectionTotalObservables);
+    var upperSectionObservables = bindScoreData('#upper-section');
+    var lowerSectionObservables = bindScoreData('#lower-section', upperSectionObservables.scoreTotalObservables);
+
+    return [ upperSectionObservables, lowerSectionObservables ].map(function (observables) {
+      return mergeObservablesByColumn(observables.scoreInputObservables, observables.scoreTotalObservables);
+    });
   }
 
   function bindScoreData(tableSelector, externalObservableByColumn) {
@@ -102,7 +118,7 @@
     });
     bindTotalData(tableSelector, totalObservablesByColumn);
 
-    return totalObservablesByColumn;
+    return { scoreInputObservables: observablesFromTable, scoreTotalObservables: totalObservablesByColumn };
   }
 
   function bindTotalData(tableSelector, totalObservablesByColumn) {
@@ -138,6 +154,42 @@
       .merge(Rx.Observable.fromEvent(inputElement, 'input')
         .map(function (e) { return +e.target.value || 0; })
         .distinctUntilChanged());
+  }
+
+
+  function observeScoreDifferences(upperSectionObservables, lowerSectionObservables) {
+    bindRowScoreData('#upper-section', upperSectionObservables);
+    bindRowScoreData('#lower-section', lowerSectionObservables);
+  }
+
+  function bindRowScoreData(tableSelector, scoreObservables) {
+    $(tableSelector + ' tbody tr').each(function (rowIndex, rowElement) {
+      var rowScoreChangeObservable = Rx.Observable.combineLatest(
+        scoreObservables[0][rowIndex],
+        scoreObservables[1][rowIndex],
+        function (score1, score2) { return RowScoreChange(score1, score2); }
+      );
+
+      // Show score difference next to bigger score cell
+      rowScoreChangeObservable.filter(function (change) { return change.score1 > change.score2; })
+        .subscribe(function (change) {
+          $(rowElement).find('.score-diff.player-1').html('+' + (change.score1 - change.score2));
+        });
+      rowScoreChangeObservable.filter(function (change) { return change.score2 > change.score1; })
+        .subscribe(function (change) {
+          $(rowElement).find('.score-diff.player-2').html('+' + (change.score2 - change.score1));
+        });
+
+      // Clear score difference cell next to score cell that is smaller or equal to the other one
+      rowScoreChangeObservable.filter(function (change) { return change.score1 <= change.score2; })
+        .subscribe(function () {
+          $(rowElement).find('.score-diff.player-1').html('');
+        });
+      rowScoreChangeObservable.filter(function (change) { return change.score2 <= change.score1; })
+        .subscribe(function () {
+          $(rowElement).find('.score-diff.player-2').html('');
+        });
+    });
   }
 
 })();
